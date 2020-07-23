a903d34b19187625728fae9dc19fa8057bfd2106
Sima <sima.boreiri@gmail.com>
2020-07-23 13:07:09 +0200

    add url if condition

--- src/index.js
+++ src/index.js
@@ -5,7 +5,13 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import axios from 'axios';
 
-axios.defaults.baseURL = "http://localhost:3000/api/v1"
+let apiUrl
+if (process.env.NODE_ENV === 'production' )  {
+  apiUrl = 'https://news-on-rails-api.herokuapp.com/api/v1'
+} else {
+  apiUrl = 'http://localhost:3000/api/v1'
+}
+axios.defaults.baseURL = apiUrl
 
 ReactDOM.render(
   <React.StrictMode>
