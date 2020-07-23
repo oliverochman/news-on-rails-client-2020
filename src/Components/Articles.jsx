7bfeca85b611d6b8acf7556029574511a31e3dd7
Alex_Bellamy <alexlbellamy@gmail.com>
2020-07-23 11:47:25 +0200

    updating articles component to also show lead and formating

--- src/Components/Articles.jsx
+++ src/Components/Articles.jsx
@@ -1,31 +1,26 @@
-import React, { Component } from 'react';
-import axios from "axios"
+import React, { Component } from "react";
+import axios from "axios";
 
 class Articles extends Component {
-    state = {
-        articles: [],
-    }
+  state = {
+    articles: [],
+  };
 
-    componentDidMount = async () => {
-        let response = await axios.get(`/articles`);
-        this.setState({ articles: response.data.articles });
-        
-    };
+  componentDidMount = async () => {
+    let response = await axios.get(`/articles`);
+    this.setState({ articles: response.data.articles });
+  };
 
   render() {
-
-
-    
     return (
-      <div 
-      > 
-   {this.state.articles.map(article => (
-       
-     <h1 id={`article-${article.id}`}
-     key={article.id} 
-     >{article.title}</h1>   
-    ))}
-     </div>
+      <div>
+        {this.state.articles.map((article) => (
+          <div id={`article-${article.id}`} key={article.id}>
+            <h1 id="title">{article.title}</h1>
+            <h2 id="lead">{article.lead}</h2>
+          </div>
+        ))}
+      </div>
     );
   }
 }
