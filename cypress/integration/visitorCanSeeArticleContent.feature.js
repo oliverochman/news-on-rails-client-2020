6adf2df92dd275968bae7c22e47b8476245a7ce8
odeane_croney <odeane_croney@yahoo.com>
2020-07-24 09:44:47 +0200

    allowing  visitor to see article

--- cypress/integration/visitorCanSeeArticleContent.feature.js
+++ cypress/integration/visitorCanSeeArticleContent.feature.js
@@ -6,17 +6,16 @@ describe('Vistors can see Article content', () => {
       url: 'http://localhost:3000/api/v1/articles',
       response: "fixture:articles_index.json",
     });
-    cy.visit("/");
-    cy.get('#article-1').within(() => {
-      cy.get('#title').should('contain', "Scrum Lord")
-      cy.get('#lead').should('contain', "Lord of all coharts")
-      cy.get('button').click()
-    })
     cy.route({
       method: 'GET',
       url: 'http://localhost:3000/api/v1/articles/1',
-      response: "fixture:article_show.json", 
+      response: "fixture:article_show.json",
     })
+    cy.visit("/");
+    cy.get('#article-1').within(() => {
+      cy.get('button').should('contain', "Read more").click()
+    })
+   
   })
   it('displays the content of the article', () => {
     cy.get('#article-1').within(() => {
@@ -24,5 +23,7 @@ describe('Vistors can see Article content', () => {
       cy.get('#lead').should('contain', "Lord of all coharts")
       cy.get('#content').should('contain', "There are fishy rumers about a guy taken the responsibility of leading all coharts due to his outstanding management skills!")
     })
+    cy.get('#article-2').should("not.exist")
+    cy.get('#article-3').should("not.exist")
   })
 })
--- src/Components/Articles.jsx
+++ src/Components/Articles.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 class Articles extends Component {
   state = {
     articles: [],
+    singleArticle: null
   };
 
   componentDidMount = async () => {
@@ -11,17 +12,45 @@ class Articles extends Component {
     this.setState({ articles: response.data.articles });
   };
 
+  getSingleArticle = async (event) => {
+    debugger;
+    let id = event.target.parentElement.dataset.id;
+    let response = await axios.get(`/articles/${id}`);
+    this.setState({ singleArticle: response.data.article });
+  };
+
+  closeSingleArticle = () => {
+    this.setState({
+      singleArticle: null
+    })
+  }
+
   render() {
-    return (
-      <div>
-        {this.state.articles.map((article) => (
-          <div id={`article-${article.id}`} key={article.id}>
+    let articles;
+    if (this.state.singleArticle) {
+      articles = (
+        <div id={`article-${this.state.singleArticle.id}`}>
+          <h1 id="title">{this.state.singleArticle.title}</h1>
+          <h2 id="lead">{this.state.singleArticle.lead}</h2>
+          <p id="content">{this.state.singleArticle.content}</p>
+          <button id="button" onClick={this.closeSingleArticle}>Close article</button>
+        </div>
+      )
+    } else {
+      articles = (
+        this.state.articles.map((article) => (
+          <div id={`article-${article.id}`} key={article.id} data-id={article.id}>
             <h1 id="title">{article.title}</h1>
             <h2 id="lead">{article.lead}</h2>
-            <button id="button">Read more</button>
+            <button id="button" onClick={this.getSingleArticle}>Read more</button>
           </div>
-        ))}
-         
+        ))
+      )
+    }
+    return (
+      <div>
+        {articles}
+
       </div>
     );
   }
