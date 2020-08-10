59ea7c4862bf0de89b65724864484dc9beafa0fe
Sima <sima.boreiri@gmail.com>
2020-08-10 13:24:11 +0200

    creates the test file and write down the test

--- cypress/integration/VisitorCanSeeArticlesCategorized.feature.js
+++ cypress/integration/VisitorCanSeeArticlesCategorized.feature.js
@@ -6,47 +6,18 @@ describe('Vistors can see Articles by Categories', () => {
       url: 'http://localhost:3000/api/v1/articles',
       response: "fixture:articles_index.json",
     });
-    cy.route({
-      method: 'GET',
-      url: 'http://localhost:3000/api/v1/articles/lifestyle',
-      response: "fixture:articles_categorized_index.json",
-    });
-    cy.route({
-      method: 'GET',
-      url: 'http://localhost:3000/api/v1/articles/lifestyle/1',
-      response: "fixture:article_show.json",
-    });
-
     cy.visit("/");
-
   });
 
   it("Visitors can see different categories", () => {
-    cy.get('button').should('contain', "economy").click()
-    cy.get('button').should('contain', "sports").click()
-    cy.get('button').should('contain', "lifestyle").click()
-
+    cy.get("#category-header");
+    cy.get("#economy").click();
+    cy.get("#article-list").should("contain", "Bright Future");
+    cy.get("#article-list").should("contain", "Management economy");
+    cy.get("#article-list").should("contain", "Bankruptcy during Corona");
+    cy.get("#article-list").should("not.contain", "Scrum Lord");
+    cy.get("#article-list").should("not.contain", "Happy Campers");
   });
-  it("Visitors can see list of articles in specific category", () => {
-    cy.get('#article-1').within(() => {
-      cy.get('#title').should('contain', "Scrum Lord")
-      cy.get('#lead').should('contain', "Lord of all coharts")
-      cy.get('button').should('contain', "Read more").click()
-    })
-    cy.get('#article-2').within(() => {
-      cy.get('#title').should('contain', "Happy Campers")
-      cy.get('#lead').should('contain', "Happy campers is always a winner")
-      cy.get('button').should('contain', "Read more").click()
-    })
 
-  });
-  it('displays the content of the article in specific category', () => {
-    cy.get('#article-1').within(() => {
-      cy.get('#title').should('contain', "Scrum Lord")
-      cy.get('#lead').should('contain', "Lord of all coharts")
-      cy.get('#content').should('contain', "A Scrum Lord punishes his coharts and rule the day with terror.")
-    })
-    cy.get('#article-2').should("not.exist")
-    cy.get('#article-3').should("not.exist")
-  })
+
 })
