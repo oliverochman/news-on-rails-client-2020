d7dc5d5d8bda5c28460320003515f54316bb5d96
Sima <sima.boreiri@gmail.com>
2020-08-16 14:13:46 +0200

    writes test for the login feature for subscriber and adds fixture file for login response

---
+++ cypress/integration/subscriberCanLoginToViewPremiumContent.feature.js
@@ -0,0 +1,48 @@
+describe("subscriber can login", () => {
+  context("successfully", () => {
+    before(() => {
+      cy.server()
+      cy.route({
+        method: "POST",
+        url: "http://localhost:3000/api/v1/auth/sign_in",
+        response: "fixture:login_response.json"
+      })
+
+      cy.route({
+        method: 'GET',
+        url: 'http://localhost:3000/api/v1/articles',
+        response: "fixture:articles_index.json",
+      })
+
+      cy.route({
+        method: "GET",
+        url: "http://localhost:3000/api/v1/articles/1",
+        response: "fixture:article_show.json"
+      })
+      cy.visit("/")
+    })
+    it("subscriber can not see article content before login", () => {
+      cy.get('#article-1').within(() => {
+        cy.get('#title').should('contain', "Scrum Lord")
+        cy.get('#lead').should('contain', "Lord of all coharts")
+      cy.get('button').should('contain', "Read more").click()
+      })
+      cy.get("login-form").should("be.visible")
+    })
+
+    it("subscriber can login", () => {
+      cy.get("button#login").click()
+      cy.get("#login-form").within(()=> {
+        cy.get("#email").type("subscriber@mail.com")
+        cy.get("#password").type("password")
+        cy.get("#login-submit").click()
+      })
+      cy.get('#article-1').within(() => {
+        cy.get('#title').should('contain', "Scrum Lord")
+        cy.get('#lead').should('contain', "Lord of all coharts")
+      cy.get('button').should('contain', "Read more").click()
+      })
+      cy.get("#content").contains("A Scrum Lord punishes his coharts and rule the day with terror.").should("be.visible")
+    })
+  })
+})
