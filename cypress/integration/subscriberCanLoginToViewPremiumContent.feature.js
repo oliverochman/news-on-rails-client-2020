42960580660bffd6eb261b6db3a1ad3c74bc3d70
emtalen <59577366+emtalen@users.noreply.github.com>
2020-08-17 15:49:58 +0200

    updating the tests so that we can implement the functionality for different articles depending on if they are premium or not

--- cypress/integration/subscriberCanLoginToViewPremiumContent.feature.js
+++ cypress/integration/subscriberCanLoginToViewPremiumContent.feature.js
@@ -11,7 +11,13 @@ describe("subscriber can login", () => {
       cy.route({
         method: "GET",
         url: "http://localhost:3000/api/v1/articles/1",
-        response: "fixture:article_show.json",
+        response: "fixture:first_article_show.json",
+      });
+
+      cy.route({
+        method: "GET",
+        url: "http://localhost:3000/api/v1/articles/2",
+        response: "fixture:second_article_show.json",
       });
 
       cy.route({
@@ -24,9 +30,20 @@ describe("subscriber can login", () => {
         method: "GET",
         url: "http://localhost:3000/api/v1/auth/**",
         response: "fixture:login_response.json",
-      })
+      });
 
       cy.visit("/");
+    });
+    it("but can see content of none premium article", () => {
+      cy.get("#article-2").within(() => {
+        cy.get("button").should("contain", "Read more").click();
+      });
+
+      cy.get('#content').should(
+        "contain",
+        "Happy campers sounds awesome and this is the content"
+      );
+      cy.get("button#login").should("not.exist");
     });
 
     it("to see content of premium article", () => {
@@ -34,6 +51,7 @@ describe("subscriber can login", () => {
         cy.get("button").should("contain", "Read more").click();
       });
       cy.get("button#login").should("be.visible");
+      cy.get('#content').should('not.exist')
       cy.get("button#login").click();
       cy.get("#login-form").within(() => {
         cy.get("#email").type("subscriber@mail.com");
@@ -45,11 +63,11 @@ describe("subscriber can login", () => {
         cy.get("#lead").should("contain", "Lord of all coharts");
         cy.get("button").should("contain", "Close article").click();
       });
-      cy.get("#content")
-        .contains(
-          "A Scrum Lord punishes his coharts and rule the day with terror."
-        )
-        .should("be.visible");
+      // cy.get("#content")
+      //   .contains(
+      //     "A Scrum Lord punishes his coharts and rule the day with terror."
+      //   )
+      //   .should("be.visible");
     });
   });
 });
