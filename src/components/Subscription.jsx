d55247431fafcb3b7fb3a76bdd9b56ea1196c015
Sima <sima.boreiri@gmail.com>
2020-08-20 11:02:16 +0200

    adds subscription component

--- src/components/Subscription.jsx
+++ src/components/Subscription.jsx
@@ -1,9 +1,60 @@
-import React from "react"
+import React, { Component } from "react";
+import axios from "axios";
+import {
+  injectStripe,
+  CardNumberElement,
+  CardExpiryElement,
+  CardCVCElement,
+} from "react-stripe-elements";
 
-const Subscription = () => {
-  return(
-    <div></div>
-  )
+class Subscription extends Component {
+  payWithStripe = async (event) => {
+    event.preventDefault();
+    let stripeResponse = await this.props.stripe.createToken();
+
+    stripeResponse.token && this.performPayment(stripeResponse.token.id);
+  };
+
+  performPayment = async (stripeToken) => {
+    let headers = await getAuthHeaders();
+    let response = await axios.post(
+      "/subscriptions",
+      {
+        stripeToken: stripeToken,
+      },
+      {
+        headers: headers,
+      }
+    );
+
+    if (response.data.paid === true) {
+      this.setState({ message: response.data.message });
+    }
+  };
+
+  render() {
+    let message;
+
+    this.state.message &&
+      (message = <p id="payment-message">{this.state.message}</p>);
+
+    return (
+      <form onSubmit={this.payWithStripe} id="payment-form">
+        <label>Card number</label>
+        <CardNumberElement />
+
+        <label>Expiry Date</label>
+        <CardExpiryElement />
+
+        <label>CVC</label>
+        <CardCVCElement />
+
+        <button id="submit-payment" type="submit">
+          Submit
+        </button>
+      </form>
+    );
+  }
 }
 
-export default Subscription;
+export default injectStripe(Subscription);
